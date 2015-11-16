276a7a0407b0977eb0ecf5cf679c18cca7bd3c18
Ben Grinberg <bengrin25@gmail.com>
2015-11-16 18:45:05 -0500

    Completed stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,37 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  if (Array.isArray(obj)) {
+    var results = [];
+
+    _.each(obj, function(x) {
+    	results.push(stringifyJSON(x));
+    });
+
+    return '[' + results.join(',') + ']';
+  }
+
+  if (obj && typeof obj === "object") {
+  	var results = [];
+
+  	_.each(obj, function(value, key) {
+  		if (typeof value === "function" || value === undefined){
+  			return "{}";
+  		}
+  		results.push(stringifyJSON(key) + ":" + stringifyJSON(value));
+  	});
+
+  	return '{' + results.join(',') + '}';
+  }
+
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  return ""+obj;
+
 };
+
+
